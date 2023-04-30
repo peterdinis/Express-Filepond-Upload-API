e10cb784fd58b73b0fbfd9738d5a1a738e5b4490
Peterdinis <peterdinis0@pobox.sk>
2023-04-30 09:52:22 +0200

    fix: FOO

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,21 +1,17 @@
 import express from "express";
 import multer from "multer";
 import { PrismaClient } from "@prisma/client";
-import * as FilePond from 'filepond';
-import filepondPluginFileEncode from "filepond-plugin-file-encode";
-import filepondPluginImagePreview from "filepond-plugin-image-preview";
+import * as FilePond from "filepond";
 
 const router = express.Router();
 
 const filepond = FilePond.create({
-    allowMultiple: false,
+  allowMultiple: false,
   allowRevert: false,
   server: {
     process: "/api/upload",
   },
 });
-filepond.registerPlugin(filepondPluginFileEncode);
-filepond.registerPlugin(filepondPluginImagePreview);
 
 const upload = multer({
   storage: multer.diskStorage({
@@ -28,27 +24,23 @@ const upload = multer({
   }),
 });
 
-router.post(
-  "/api/upload",
-  upload.single("filepond"),
-  async (req, res) => {
-    try {
-      const prisma = new PrismaClient();
-      const { filename, path } = req.file;
+router.post("/api/upload", upload.single("filepond"), async (req, res) => {
+  try {
+    const prisma = new PrismaClient();
+    const { filename, path } = req.file;
 
-      // Save the file to the database
-      const savedFile = await prisma.file.create({
-        data: {
-          name: filename,
-          path,
-        },
-      });
+    // Save the file to the database
+    const savedFile = await prisma.file.create({
+      data: {
+        name: filename,
+        path,
+      },
+    });
 
-      res.status(200).json({ id: savedFile.id });
-    } catch (err) {
-      res.status(500).json({ error: err.message });
-    }
+    res.status(200).json({ id: savedFile.id });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
-);
+});
 
 export default router;
