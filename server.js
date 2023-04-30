7c9c4903a6307bc680129803021b5a42096055a0
Peterdinis <peterdinis0@pobox.sk>
2023-04-30 09:28:43 +0200

    fix: Fixing bugs in api wip

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import uploadRoutes from './routes/uploadRoutes.js'
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(uploadRoutes);
 dotenv.config();
 
 app.listen(8500, () => {
