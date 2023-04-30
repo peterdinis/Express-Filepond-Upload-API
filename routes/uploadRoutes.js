cc43115b32b1faabb2f5bf2f6f1ee4f5d3ed6a95
Peterdinis <peterdinis0@pobox.sk>
2023-04-30 13:52:05 +0200

    FOO

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -4,20 +4,9 @@ import {
   getPhotoDetail,
   uploadNewPhoto,
 } from "../controller/uploadController.js";
-import multer from "multer";
+import {upload} from "../middleware/multer.js"
 
 const router = express.Router();
-
-export const upload = multer({
-  storage: multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, "./uploads");
-    },
-    filename: (req, file, cb) => {
-      cb(null, `${Date.now()}-${file.originalname}`);
-    },
-  }),
-});
 
 router.get("/api/uploads/all", getAllPhotos);
 
