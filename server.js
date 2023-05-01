1dd34dd26eb8096de4b438728bf47add77836286
Peterdinis <peterdinis0@pobox.sk>
2023-05-01 09:23:08 +0200

    feat: UPdating api

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import uploadRoutes from './routes/uploadRoutes.js'
+import morgan from "morgan";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan('tiny'));
 app.use(uploadRoutes);
 dotenv.config();
 
