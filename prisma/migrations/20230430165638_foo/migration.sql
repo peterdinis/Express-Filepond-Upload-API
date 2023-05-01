315c97703272576ed97b1d10d9b1873c41e8d33f
Peterdinis <peterdinis0@pobox.sk>
2023-05-01 18:33:42 +0200

    feat: Change morgan middleware to dev

--- prisma/migrations/20230430165638_foo/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `customId` on the `File` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "File" DROP COLUMN "customId",
-ADD COLUMN     "externalId" TEXT NOT NULL DEFAULT '1000';
