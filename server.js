d261c878184482d97cbf21c5b3dfdce1508a9ca7
Peterdinis <peterdinis0@pobox.sk>
2023-10-28 21:03:15 +0200

    feat: Small server update

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import uploadRoutes from './routes/uploadRoutes.js';
-import exampleRoutes from './routes/exampleRoutes.js';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import uploadRoutes from "./routes/uploadRoutes.js";
+import exampleRoutes from "./routes/exampleRoutes.js";
 import morgan from "morgan";
+import chalk from "chalk";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 app.use(uploadRoutes);
 app.use(exampleRoutes);
 
 dotenv.config();
 
-const PORT = process.env.PORT || 8500
+const PORT = process.env.PORT || 8500;
 
 app.listen(PORT, () => {
-    console.log(`Applikácia beží na porte ${PORT}`);
-})
+  console.log(chalk.blue(`Applikácia beží na porte ${PORT}`));
+});
