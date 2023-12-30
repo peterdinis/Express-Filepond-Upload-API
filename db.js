c36eec2c84cc9d5c5e4fe603bafcc680ea3b6362
Peterdinis <peterdinis0@pobox.sk>
2023-12-30 21:52:31 +0100

    infra: Prisma update

--- db.js
+++ db.js
@@ -3,5 +3,3 @@ import { PrismaClient } from "@prisma/client";
 const db = new PrismaClient();
 
 export default db;
-
-// TODO: This code work only in development in producion this must be little bit different
